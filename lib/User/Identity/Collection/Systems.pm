457c4b48888fcd3dbb8dd839e4dea634ea8ee177
Mark Overmeer <mark@overmeer.net>
2007-07-25 06:53:59 +0000

    Import of MARKOV/User-Identity-0.92 from CPAN.
    gitpan-cpan-distribution: User-Identity
    gitpan-cpan-version:      0.92
    gitpan-cpan-path:         MARKOV/User-Identity-0.92.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/User/Identity/Collection/Systems.pm
+++ lib/User/Identity/Collection/Systems.pm
@@ -1,10 +1,10 @@
 # Copyrights 2003,2004,2007 by Mark Overmeer <perl@overmeer.net>.
-# For other contributors see Changes.
+#  For other contributors see Changes.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 0.99.
+# Pod stripped from pm file by OODoc 1.02.
 package User::Identity::Collection::Systems;
 use vars '$VERSION';
-$VERSION = '0.91';
+$VERSION = '0.92';
 use base 'User::Identity::Collection';
 
 use strict;
